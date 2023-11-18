f168ea090d7e0640d734dfe9495de286ce4cebff
mLotz <67031290+MariusLotz@users.noreply.github.com>
2023-11-18 19:22:20 +0000

    trainingsdata created

--- InputData/sin_cos_creator.py
+++ InputData/sin_cos_creator.py
@@ -1,25 +1,112 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+import pickle
 
-
-# Define the synthetic time series generator function
-def generate_synthetic_data(num_points=100, noise_level=0.1):
+def sin_cos_creator(num_points=25, number_basis_functions=10):
     time = np.arange(num_points)
-    noise = np.random.normal(0, noise_level, num_points)
-    sine_wave = 2.1 * np.sin(2 * np.pi * time / 21)  # Sinusoidal pattern
-    cosine_wave = 1.7 * np.cos(2 * np.pi * time / 13)  # Cosine pattern
+    cos_signal = np.zeros(num_points)
+    for i in range(number_basis_functions):
+        amplitude  = random.uniform(0.1, 10.0)
+        frequence = random.uniform(0.1, 10.0)
+        phase = random.uniform(-0.25, 0.25)
+        cos_wave = amplitude * np.cos(2 * np.pi * (time + phase) * frequence)  # Cosine pattern
+        cos_signal = cos_signal + cos_wave
+    return cos_signal
 
-    # Generate the synthetic time series data
-    synthetic_data = sine_wave + cosine_wave + noise
-    return synthetic_data
+def sin_cos_creator2(num_points=25, number_basis_functions=100):
+    time = np.arange(num_points)
+    cos_signal = np.zeros(num_points)
+    for i in range(number_basis_functions):
+        amplitude  = random.uniform(0.66, 1)
+        frequence = random.uniform(1, 2)
+        phase = random.uniform(-0.25, 0.25)
+        cos_wave = amplitude * np.cos(2 * np.pi * (time + phase) * frequence)  # Cosine pattern
+        cos_signal = cos_signal + cos_wave
+    return cos_signal / np.sqrt(number_basis_functions)
 
-# Generate synthetic time series data
-synthetic_data = generate_synthetic_data()
+def sin_cos_creator3(num_points=100, number_basis_functions=10000):
+    time = np.arange(num_points)
+    cos_signal = np.zeros(num_points)
+    for i in range(number_basis_functions):
+        amplitude  = random.uniform(0.66, 1)
+        frequence = random.uniform(0.5, 2)
+        phase = random.uniform(-0.33, 0.33)
+        cos_wave = amplitude * np.cos(2 * np.pi * (time + phase) * frequence)  # Cosine pattern
+        cos_signal = cos_signal + cos_wave
+    return cos_signal / np.sqrt(number_basis_functions)
 
-# Plot the synthetic time series data
-plt.figure(figsize=(10, 6))
-plt.plot(synthetic_data)
-plt.title('Synthetic Time Series Data with Sinusoidal and Cosine Patterns')
-plt.xlabel('Time')
-plt.ylabel('Value')
-plt.savefig('sample_plot.png')
+def sin_cos_creator4(num_points=100, number_basis_functions=100):
+    time = np.arange(num_points)
+    cos_signal = np.zeros(num_points)
+    for i in range(number_basis_functions):
+        amplitude  = random.uniform(0.75, 1)
+        frequence = random.uniform(0.75, 1.25)
+        phase = random.uniform(-0.25, 0.25)
+        cos_wave = amplitude * np.cos(2 * np.pi * (time + phase) * frequence)  # Cosine pattern
+        cos_signal = cos_signal + cos_wave
+    cos_signal[0] = 0
+    return cos_signal / np.sqrt(number_basis_functions)
+
+def sin_cos_creator5(num_points=100, number_basis_functions=23):
+    time = np.arange(num_points)
+    cos_signal = np.zeros(num_points)
+    for i in range(number_basis_functions):
+        amplitude  = random.uniform(0.33, 3)
+        frequence = random.uniform(3, 3.3)
+        phase = random.uniform(-0.25, 0.25)
+        cos_wave = amplitude * np.cos(2 * np.pi * (time + phase) * frequence)  # Cosine pattern
+        cos_signal = cos_signal + cos_wave
+    cos_signal[0] = 0
+    return cos_signal / np.sqrt(number_basis_functions)
+
+def sin_cos_creator6(num_points=100, number_basis_functions=13):
+    time = np.arange(num_points)
+    cos_signal = np.zeros(num_points)
+    for i in range(number_basis_functions):
+        amplitude  = random.uniform(0.5, 1)
+        frequence = random.uniform(3, 3.3)
+        phase = random.uniform(-0.13, 0.13)
+        cos_wave = amplitude * np.cos(2 * np.pi * (time + phase) * frequence)  # Cosine pattern
+        cos_signal = cos_signal + cos_wave
+    cos_signal[0] = 0
+    return cos_signal / np.sqrt(number_basis_functions)
+
+
+def grapfical_print():
+    # Generate synthetic time series data
+    synthetic_data = sin_cos_creator6()
+
+    # Plot the synthetic time series data
+    plt.figure(figsize=(10, 6))
+    plt.plot(synthetic_data)
+    plt.title('Synthetic Time Series Data with Sinusoidal and Cosine Patterns')
+    plt.xlabel('Time')
+    plt.ylabel('Value')
+    plt.savefig('sample_plot1.png')
+
+def create_sample(n, name):
+    sample = []
+    for i in range(n):
+        sample.append(sin_cos_creator6())
+        print(i)
+
+    # Save data to a file using pickle
+    with open(name + '.pkl', 'wb') as file:
+        pickle.dump(sample, file)
+
+def print_load_file():
+    # Load data from the file
+    with open('training_sample_sin_cos_creator6.pkl', 'rb') as file:
+        loaded_data = pickle.load(file)
+
+    #Display the loaded data
+    print("Loaded Data:", loaded_data)
+
+
+if __name__=="__main__":
+    create_sample(10000, "10000_test_sample_sin_cos_creator6")
+    #print_load_file()
+    
+
+    
